bf2b45e662c4424347ecc6a8a78c9932a4f9d352
dhruva <dhruva3223@gmail.com>
2022-05-11 10:14:26 +0530

    init

---
+++ start_video.py
@@ -0,0 +1,110 @@
+# import the necessary packages
+from email import message
+from keras.applications.mobilenet_v2 import preprocess_input
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage, messagebox
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
+from imutils.video import VideoStream
+from subprocess import call
+import numpy as np
+import imutils
+import time
+import cv2
+import os
+from tkinter.simpledialog import askstring
+from tkinter.messagebox import showinfo
+
+
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+		(104.0, 177.0, 123.0))
+
+	faceNet.setInput(blob)
+	detections = faceNet.forward()
+
+	faces = []
+	locs = []
+	preds = []
+
+	for i in range(0, detections.shape[2]):
+		confidence = detections[0, 0, i, 2]
+		if confidence > 0.5:
+			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+			(startX, startY, endX, endY) = box.astype("int")
+			(startX, startY) = (max(0, startX), max(0, startY))
+			(endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+			face = frame[startY:endY, startX:endX]
+			face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+			face = cv2.resize(face, (224, 224))
+			face = img_to_array(face)
+			face = preprocess_input(face)
+			faces.append(face)
+			locs.append((startX, startY, endX, endY))
+
+	if len(faces) > 0:
+		faces = np.array(faces, dtype="float32")
+		preds = maskNet.predict(faces, batch_size=32)
+
+	return (locs, preds)
+
+def run():
+	name = askstring('Camera Option', 'Enter Ip to connect to another camera')
+	prototxtPath = r"face_detector\deploy.prototxt"
+	weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+	faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+	maskNet = load_model("MiniProject.model")
+
+	print("[INFO] starting video stream...")
+	if len(name)>0:
+		vs = VideoStream(f"{name}/video").start()
+	else:
+		vs = VideoStream(src=0).start()
+
+	root = Tk()
+	root.overrideredirect(1)
+	root.withdraw()
+	messagebox.showinfo("Info", "press q to close the camera")
+
+
+	while True:
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
+		(locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+		for (box, pred) in zip(locs, preds):
+			(startX, startY, endX, endY) = box
+			(incorrect_mask,with_mask, without_mask) = pred
+
+            
+			if without_mask > 0.82:
+				label = "No mask"
+			elif with_mask > 0.7:
+				label = "mask"
+			else:
+				label = "Wear mask correctly"
+
+			if label == "mask":
+				color = (0, 255, 0)
+
+			elif label == "Wear mask correctly":
+				color = (0, 255, 255)
+			else:
+				color = (0, 0, 255)
+
+			label = "{}: {:.2f}%".format(label, max(incorrect_mask, with_mask, without_mask) * 100)
+			cv2.putText(frame, label, (startX, startY - 10),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+			cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		if key == ord("q"):
+			break
+
+	cv2.destroyAllWindows()
+	vs.stop()
+	vs.stream.release()
+	call('python __main__.py', shell=True)
